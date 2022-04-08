e043fb3d366c7239a43004549593025f18b05881
Alex <chaza.mckay@me.com>
2022-04-08 17:27:45 +0930

    basic finish

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # time-on
+
+## Description
+
+Work at the Airport? Need to record flight data for arrivals and departures?
+This app is a work in progress, currently functions on a very basic level but it will assist us in recording times which otherise take us a long time to write down.
+
+## Resources 
+
+[Repo]()
+[Live Site]()
---
+++ script.js
@@ -0,0 +1,72 @@
+const menuBtn = document.getElementById('menu-btn');
+const turnBtn = document.getElementById('turn-btn');
+const arrivalBtn = document.getElementById('arrival-btn');
+const departureBtn = document.getElementById('departure-btn');
+const clear = document.getElementById('clear-btn');
+
+const menu = document.getElementById('menu');
+const turn = document.getElementById('turn');
+const arrival = document.getElementById('arrival');
+const departure = document.getElementById('departure');
+
+const menuHandler = () => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        turn.classList.add('hidden');
+        arrival.classList.add('hidden');
+        departure.classList.add('hidden');
+        return;
+    }
+    menu.classList.add('hidden');
+}
+
+const turnHandler = () => {
+    if (turn.classList.contains('hidden')) {
+        turn.classList.remove('hidden');
+        menu.classList.add('hidden');
+        return;
+    }
+    turn.classList.add('hidden');
+}
+
+const arrivalHandler = () => {
+    if (arrival.classList.contains('hidden')) {
+        arrival.classList.remove('hidden');
+        menu.classList.add('hidden');
+        return;
+    }
+    arrival.classList.add('hidden');
+}
+
+const departureHandler = () => {
+    if (departure.classList.contains('hidden')) {
+        departure.classList.remove('hidden');
+        menu.classList.add('hidden');
+        return;
+    }
+    departure.classList.add('hidden');
+}
+
+const getCurrentTime = () => {
+    const current = new Date();
+    return current.toLocaleTimeString();
+}
+
+const saveTime = (event) => {
+    event.target.textContent = getCurrentTime();
+}
+
+const timeBtn = document.querySelectorAll(".time")
+timeBtn.forEach((currentBtn) => {
+    currentBtn.addEventListener('click', saveTime);
+});
+
+const clearHandler = () => {
+    location.reload();
+}
+
+clear.addEventListener('click', clearHandler)
+menuBtn.addEventListener('click', menuHandler);
+turnBtn.addEventListener('click', turnHandler);
+arrivalBtn.addEventListener('click', arrivalHandler);
+departureBtn.addEventListener('click', departureHandler);
